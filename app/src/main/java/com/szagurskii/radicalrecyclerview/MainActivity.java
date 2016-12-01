07a47fdf1040a05dae1c0b2c3f3839609ee035b9
Savelii Zagurskii <saveliyzagurskiy@gmail.com>
2016-12-01 18:58:23 +0300

    Now setting a specific title of the activity.

--- app/src/main/java/com/szagurskii/radicalrecyclerview/MainActivity.java
+++ app/src/main/java/com/szagurskii/radicalrecyclerview/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends Activity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
     ButterKnife.bind(this);
+    setTitle(R.string.app_name);
 
     basicAdapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
       @Override public void onChanged() {
